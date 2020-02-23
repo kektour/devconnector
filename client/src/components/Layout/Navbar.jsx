c0c52a000445ec64d9cad49e3cb726ead69764a6
Alexey <ylexe_97@gmail.com>
2020-02-23 11:30:05 +0300

    Initial commit

---
+++ client/src/components/Layout/Navbar.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
+import { clearProfile } from '../../actions/profileActions';
+
+class Navbar extends Component {
+  static propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    clearProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
+
+  onLogoutClick = e => {
+    e.preventDefault();
+
+    this.props.clearProfile();
+    this.props.logoutUser();
+  };
+
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a href="" className="nav-link" onClick={this.onLogoutClick}>
+            <img
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: '25px', marginRight: '5px' }}
+              className="rounded-circle"
+              title="You must have a Gravatat connected to your email to display an image"
+            />{' '}
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            DevConnector
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles">
+                  Developers
+                </Link>
+              </li>
+            </ul>
+            {isAuthenticated ? authLinks : guestLinks}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser, clearProfile })(Navbar);
